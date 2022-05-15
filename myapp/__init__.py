e2066740e2724646d05c079543a69f6f8edda95b
yaguruma <yaguruma_y@yahoo.co.jp>
2022-05-15 14:09:33 +0900

    [update] アプリをファクトリ関数にまとめる変更

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,7 +1,28 @@
+import os
+
 from flask import Flask, render_template
 
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html', name='hoge')
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        # root/instance/config.py を読み込む。存在しなければ無視する
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def hello_world():
+        return render_template('index.html', name='fuga')
+
+    return app
+
+
+# gunicornで呼び出すアプリケーションをappに代入
+app = create_app()
