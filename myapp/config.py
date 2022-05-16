31d29894d9a1477a0c728f8bbb30fbc3cdc6ba50
yaguruma <yaguruma_y@yahoo.co.jp>
2022-05-16 22:18:29 +0900

    [update] PostgreSQLの接続確認ができたのでDBをSQLiteに戻す変更

--- myapp/config.py
+++ myapp/config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = 'dev'
-# SQLALCHEMY_DATABASE_URI='sqlite:///myappdb.sqlite3'
-SQLALCHEMY_DATABASE_URI='postgresql://localhost/flasknote'
+SQLALCHEMY_DATABASE_URI='sqlite:///myappdb.sqlite3'
+# SQLALCHEMY_DATABASE_URI='postgresql://localhost/flasknote'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
