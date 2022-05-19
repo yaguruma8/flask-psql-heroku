0336b05dcf6bc00be0e78f7d66ac0e530853b44f
yaguruma <yaguruma_y@yahoo.co.jp>
2022-05-19 22:21:11 +0900

    [update] 設定値を環境変数から読み出すようにする変更

--- myapp/config.py
+++ myapp/config.py
@@ -1,5 +1,13 @@
-SECRET_KEY = 'dev'
+import os
+
+SECRET_KEY = os.getenv('MY_SECRET')
 # SQLALCHEMY_DATABASE_URI='sqlite:///myappdb.sqlite3'
 # SQLALCHEMY_DATABASE_URI='postgresql://localhost/flasknote'
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:pass@localhost/flasknote'
-SQLALCHEMY_TRACK_MODIFICATIONS=False
+local_uri = 'postgresql://{user}:{pass}@{host}/{dbname}'.format(**{
+    'user': os.getenv('DB_USERNAME', 'root'),
+    'pass': os.getenv('DB_PASSWORD', ''),
+    'host': os.getenv('DB_HOST', 'localhost'),
+    'dbname': os.getenv('DB_NAME'),
+})
+SQLALCHEMY_DATABASE_URI = local_uri
+SQLALCHEMY_TRACK_MODIFICATIONS = False
