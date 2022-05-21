36a62494f180ceed2f0a296067e29c142e033a20
yaguruma <yaguruma_y@yahoo.co.jp>
2022-05-21 17:02:28 +0900

    [fix] HerokuのPostgreSQLのURIを変換する際の条件式の不備を修正

--- myapp/config.py
+++ myapp/config.py
@@ -8,7 +8,7 @@ local_uri = 'postgresql://{user}:{pass}@{host}/{dbname}'.format(**{
     'dbname': os.getenv('DB_NAME'),
 })
 heroku_db_uri = os.getenv('DATABASE_URL')
-if heroku_db_uri.startswith("postgres://"):
+if heroku_db_uri is not None and heroku_db_uri.startswith("postgres://"):
     heroku_db_uri = heroku_db_uri.replace("postgres://", "postgresql://", 1)
 
 SQLALCHEMY_DATABASE_URI = heroku_db_uri or local_uri
